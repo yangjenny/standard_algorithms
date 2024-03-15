f82606b82e94f89227265f1cca9a7ed284bc5996
yangjenny <yangjy259@gmail.com>
2024-03-15 10:57:49 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="standard_algorithms", 
+    version="1",
+    author="Jenny Yang",
+    author_email="jenny.yang@eng.ox.ac.uk",
+    description="standard_algorithms provides automatic training of 3 classifiers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/yangjenny/standard_algorithms/",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent'
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        'xgboost',
+        'numpy',
+        'pandas',
+        'sklearn',
+        'torch'
+    ]
+)
